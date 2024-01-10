0688883d24d6d1609e0dafd6f86ff31e522c5e08
mwises <88902469+mwises@users.noreply.github.com>
2024-01-10 17:03:01 -0300

    Componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,24 @@
-import { Menu } from "./Menu";
+import { Box } from "@chakra-ui/react";
+import { Logo } from "../Common/Logo";
 
 export function Header() {
-    return (
-        <Menu />
-    );
-}
+  return (
+    <Box
+      width="100%"
+      bgGradient={["linear(180deg, #050607 0%, #05060700 100%)"]}
+      position="fixed"
+      zIndex="1000"
+    >
+      <Box
+        margin="10px auto"
+        maxWidth="1200px"
+        padding="10px 10px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Logo />
+      </Box>
+    </Box>
+  );
+};
