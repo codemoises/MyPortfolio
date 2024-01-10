2cae50f798d5ffa4ad1ef5a1cdb375600190e82c
mwises <88902469+mwises@users.noreply.github.com>
2024-01-10 17:02:42 -0300

    Componente Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,19 @@
-import FooterComponent from "./FooterComponent";
+import { Box, Link, Flex, Text } from "@chakra-ui/react";
 
+let year = new Date().getFullYear();
 export default function Footer() {
     return (
-        <FooterComponent />
+        <Flex width={"100%"} justifyContent={"center"} alignItems={"center"} height={"60px"} borderTop={"1px solid #282c31"} textAlign={"center"}>
+            <Box fontSize={"12px"} fontFamily={"Open Sans, sans-serif;"} color={"#c9c9c9"} display={"flex"} gap={"3px"}>
+                ©
+                <Text>
+                    {year}
+                </Text>
+                <Link href="http://github.com/mwises" _hover={{textDecoration: "none"}}>
+                    Moises Cruz
+                </Link>
+            </Box>
+        </Flex>
     );
 }
+
