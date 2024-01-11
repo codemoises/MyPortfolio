09e45be59b72da04b3eadf9042ea169ecf8cc946
mwises <88902469+mwises@users.noreply.github.com>
2024-01-11 18:55:47 -0300

    Update AnimateFile.tsx

--- src/components/Common/AnimateFile.tsx
+++ src/components/Common/AnimateFile.tsx
@@ -5,6 +5,7 @@ export default function AnimateFile() {
     <Box
       className="blink"
       fontSize={["20px", "30px", "30px", "40px", "40px", "50px"]}
+      fontWeight={"400"}
       display="inline-block"
     >
       <span>|</span>
