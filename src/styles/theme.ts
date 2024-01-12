a6c1aafe6207d79db597207eb1ed7f82b6455458
mwises <88902469+mwises@users.noreply.github.com>
2024-01-12 19:49:01 -0300

    Nova cor do background

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,7 +16,7 @@ export const theme = extendTheme ({
     styles: {
         global: {
             body: {
-                bg: "#06040c",
+                bg: "#0e0e0e",
                 color:"white"
             }
         }
