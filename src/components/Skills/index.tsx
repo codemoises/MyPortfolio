17560d106fc567d72132cfee415fe0ce6ce26379
mwises <88902469+mwises@users.noreply.github.com>
2024-01-12 19:52:41 -0300

    Section Conhecimentos

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,7 +1,6 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { Container } from "../Common/Container";
 import { TextContent } from "../Common/DefaultText";
-import CardSkill from "../SkillsCard";
+import SkillsCard from "../SkillsCard";
 
 export default function ContentSkills() {
   return (
@@ -17,56 +16,31 @@ export default function ContentSkills() {
     ]}>
       <Box marginTop={["0", "0", "24px"]}>
         <Box padding={["0 0 0 10px", "0 0 0 10px", 0, 0, 0]}>
-          <TextContent width="fit-content" text={"Conhecimentos"} />
+          <TextContent fontSize={["18px", "18px", "18px", "18px", "19px", "20px"]} text={"Conhecimentos"} />
         </Box>
-        <Flex flexDirection="column" gap={["65px", "65px", "45px", "20px"]} padding={["8px 8px 0 8px", "16px 0 0 0", "16px 0 0 0", "16px 0 0 0", "16px 0 0 25px"]}>
-          <CardSkill
+        <Flex maxWidth={"588px"} flexWrap={"wrap"}  gap={["32px", "65px", "28px", "28px", "28px"]} padding={["8px 8px 0 8px", "16px 0 0 0", "16px 0 0 0", "16px 0 0 0", "16px 0 0 25px"]}>
+          <SkillsCard
             img={"/assets/Image/javascript.svg"}
-            alt={"Logo do JavaScript"}
-            title={"JavaScript"}
-            description={
-              "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web."
-            }
-          />
-          <CardSkill
+            alt={"Logo do JavaScript"}        />
+          <SkillsCard
             img={"assets/Image/typescript.svg"}
             alt={"Logo do TypeScript"}
-            title={"TypeScript"}
-            description={
-              "TypeScript é uma linguagem de programação de código aberto desenvolvida pela Microsoft. É um superconjunto sintático estrito de JavaScript e adiciona tipagem estática opcional à linguagem."
-            }
           />
-          <CardSkill
+          <SkillsCard
             img={"assets/Image/react.svg"}
             alt={"Logo do react"}
-            title={"React.js"}
-            description={
-              "O React é uma biblioteca front-end JavaScript de código aberto com foco em criar interfaces de usuário em páginas web."
-            }
           />
-          <CardSkill
+          <SkillsCard
             img={"assets/Image/next.svg"}
             alt={"Logo do next"}
-            title={"Next.js"}
-            description={
-              "Next é uma empresa brasileira de tecnologia, que atua como fintech e operadora de cartões de crédito e débito, com sede na cidade de São Paulo, no estado homônimo."
-            }
           />
-          <CardSkill
+          <SkillsCard
             img={"assets/Image/html5.svg"}
             alt={"Logo do html5"}
-            title={"HTML5"}
-            description={
-              "HTML5 é uma linguagem de marcação para a World Wide Web e é uma tecnologia chave da Internet, originalmente proposto por Opera Software. É a quinta versão da linguagem HTML."
-            }
           />
-          <CardSkill
+          <SkillsCard
             img={"assets/Image/css3.svg"}
             alt={"Logo do css3"}
-            title={"CSS3"}
-            description={
-              "CSS3 é a terceira mais nova versão das famosas Cascading Style Sheets, pela qual se define estilos para um projeto web. Com efeitos de transição, imagem, imagem de fundo/background e outros, pode-se criar estilos únicos para seus projetos web, alterando diversos aspectos de design no layout da página."
-            }
           />
         </Flex>
       </Box>
--- src/components/SkillsCard/index.tsx
+++ src/components/SkillsCard/index.tsx
@@ -1,42 +1,20 @@
-import { Flex, Image, Box } from "@chakra-ui/react";
-import { TextContentThree, TextContentTwo } from "../Common/DefaultText";
+import { Flex, Box } from "@chakra-ui/react";
 import { ImageComponent } from "../ImageComponent";
 
 interface CardProps {
   img: string;
   alt: string;
-  title: string;
-  description: string;
 }
 
-export default function CardSkill({ img, alt, title, description }: CardProps) {
+export default function SkillsCard({ img, alt }: CardProps) {
   return (
     <Flex
       flexDirection={"row"}
       alignItems="center"
     >
-      <Box display={"flex"} margin={["0", "0", "0px 8px 0 0px", "0px 8px 0 0px", "0px 8px 0 0px"]}
-             padding={["0", "0", "0px 8px 0 0px", "0px 8px 0 0px", "0px 8px 0 0px"]}>
-        <ImageComponent width="85px" height="85px" src={img} alt={alt} />
+      <Box>
+        <ImageComponent width={"144.83px"} src={img} alt={alt} />
       </Box>
-      <Flex
-        flexDirection="column"
-        width={["330px", "530px", "600px", "800px", "900px", "960px"]}
-        margin={"8px 8px 0 8px"}
-        padding={"8px 8px 0 4px"}
-        alignItems={"start"}
-      >
-        <TextContentThree
-          text={title}
-        />
-        <TextContentTwo
-          color={"#a2a3a5"}
-          fontSize={["12px", "16px"]}
-          letterSpacing={"0.3px"}
-          lineHeight={"140%"}
-          text={description}
-        />
-      </Flex>
     </Flex>
   );
 }
