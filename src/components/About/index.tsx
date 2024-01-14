04408f26e076c0b73287c370d9d8a88985bbde27
mwises <88902469+mwises@users.noreply.github.com>
2024-01-14 15:55:06 -0300

    Update index.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,21 +5,32 @@ import ImagesGrid from "../AboutImage";
 export function About() {
   return (
     <>
-      <Box id="about" margin="0 auto"
-    maxWidth="1200px"
-    padding={[
-      "132px 0px 32px 0px",
-      "132px 8px 45px 8px",
-      "96px 24px 45px 24px",
-      "96px 16px 157px 16px",
-      "96px 16px 157px 16px",
-      "96px 16px 157px 16px",
-    ]}>
-        <Flex alignItems="center" justifyContent="space-between" flexDirection={["column", "column", "column", "column", "row"]}>
-          <AboutGrid />
-          <ImagesGrid />
+      <Box 
+        id="about"
+        margin="0 auto"
+        maxWidth="1200px"
+        padding={[
+          "132px 0px 32px 0px",
+          "132px 8px 45px 8px",
+          "96px 24px 45px 24px",
+          "96px 16px 157px 16px",
+          "96px 16px 157px 16px",
+          "96px 16px 157px 16px",
+      ]}>
+        <Flex 
+          alignItems="center" 
+          justifyContent="space-between" 
+          flexDirection={[
+            "column", 
+            "column", 
+            "column", 
+            "column", 
+            "row"
+            ]}>
+            <AboutGrid />
+            <ImagesGrid />
         </Flex>
       </Box>
     </>
   );
-}
+};
