6d636eda8ecf03ced306fc4f36e9f9ed8f967183
mwises <88902469+mwises@users.noreply.github.com>
2024-01-16 15:16:52 -0300

    Update index.tsx

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,5 +1,4 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { Container } from "../Common/Container";
 import { TextContent } from "../Common/DefaultText";
 import CardProjects from "../ProjectsCard";
 
@@ -26,25 +25,23 @@ export default function Projects() {
           flexWrap={"wrap"}
         >
           <CardProjects 
-            title={"BlueTalks"} 
+            title={"BlueTalks"}
             link={"https://github.com/SaulloPontes/Bluetalks"}
             description={"Uma prancheta virtual que facilite a comunicação de pessoas com deficiência."}
-            alt={"Imagem do Bluetalks"} 
-            src={"/assets/Image/bluetalks.svg"} />
+            alt={"Imagem do Bluetalks"}
+            src={"/assets/Image/bluetalks.svg"}          />
 
           <CardProjects
             title={"Calculadora"}
             description={"Calculadora simples criada com HTML5, CSS3 e JavaScript."}
             link={"https://calculadora-theta.vercel.app/"}
-            src={"/assets/Image/calculadora.svg"}
-          alt={"Imagem da calculadora"}          />
+            src={"/assets/Image/calculadora.svg"} alt={"Imagem da calculadora"}          />
 
           <CardProjects
             title={"Landing Page"}
             description={"Landing page para implementar habilidades em HTML5 e CSS3."}
             link={"https://landing-page-gamma-five.vercel.app/"}
-            src={"/assets/Image/landingPage.svg"}
-            alt={"Imagem da Landing Page"}          />
+            src={"/assets/Image/landingPage.svg"} alt={"Imagem da landing page"}          />
 
           <CardProjects title={"Spotify Clone"} link={"https://spotify-clone-dev-web.vercel.app/"} description={"Clone do Spotify. Feito em colaboração com alguns amigos."} alt={"Imagem da logo do Spotify"} src={"/assets/Image/spotifyClone.svg"} />
         </Flex>
