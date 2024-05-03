ffcb0f3dd14706b735af7f2eb91105969103d8db
Moises Cruz Braga <moisesbraga223@gmail.com>
2024-05-03 13:21:35 -0300

    remove fragmento desnecessário

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -5,55 +5,53 @@ import { TextContent } from "../Common/DefaultText";
 
 export function About() {
   return (
-    <>
-      <Box
-        id="about"
-        margin="0 auto"
-        maxWidth="1200px"
+    <Box
+      id="about"
+      margin="0 auto"
+      maxWidth="1200px"
+      padding={[
+        "42px 8px 64px 8px",
+        "42px 25px 64px 25px",
+        "42px 25px 64px 25px",
+        "96px 16px 96px 16px",
+        "96px 16px 96px 16px",
+        "96px 16px 96px 16px",
+      ]}
+    >
+      <Box padding={["0 0 0 10px", 0, 0, 0, 0]}>
+        <TextContent
+          fontWeight={"300"}
+          fontSize={["18px", "18px", "18px", "18px", "24px", "24px"]}
+          text="Sobre mim"
+        />
+      </Box>
+      <Flex
+        alignItems={[
+          "center",
+          "flex-start",
+          "flex-start",
+          "flex-start",
+          "flex-start",
+        ]}
+        justifyContent={[
+          "center",
+          "flex-start",
+          "flex-start",
+          "flex-start",
+          "space-between",
+        ]}
+        flexDirection={["column", "column", "column", "column", "row"]}
         padding={[
-          "42px 8px 64px 8px",
-          "42px 25px 64px 25px",
-          "42px 25px 64px 25px",
-          "96px 16px 96px 16px",
-          "96px 16px 96px 16px",
-          "96px 16px 96px 16px",
+          "32px 8px 0 8px",
+          "16px 0 0 0",
+          "16px 0 0 0",
+          "16px 0 0 0",
+          "32px 25px 0 25px",
         ]}
       >
-        <Box padding={["0 0 0 10px", 0, 0, 0, 0]}>
-          <TextContent
-            fontWeight={"300"}
-            fontSize={["18px", "18px", "18px", "18px", "24px", "24px"]}
-            text="Sobre mim"
-          />
-        </Box>
-        <Flex
-          alignItems={[
-            "center",
-            "flex-start",
-            "flex-start",
-            "flex-start",
-            "flex-start",
-          ]}
-          justifyContent={[
-            "center",
-            "flex-start",
-            "flex-start",
-            "flex-start",
-            "space-between",
-          ]}
-          flexDirection={["column", "column", "column", "column", "row"]}
-          padding={[
-            "32px 8px 0 8px",
-            "16px 0 0 0",
-            "16px 0 0 0",
-            "16px 0 0 0",
-            "32px 25px 0 25px",
-          ]}
-        >
-          <AboutGrid />
-          <ImagesGrid />
-        </Flex>
-      </Box>
-    </>
+        <AboutGrid />
+        <ImagesGrid />
+      </Flex>
+    </Box>
   );
 }
--- src/components/Social/index.tsx
+++ src/components/Social/index.tsx
@@ -3,35 +3,33 @@ import SocialIconComponent from "../SocialIconComponent";
 
 export function Social() {
   return (
-    <>
-      <Box marginTop={"40px"}>
-        <Flex
-          className={"social"}
-          width={["auto", "auto", "550px", "550px", "auto"]}
-          justifyContent={"space-between"}
-          flexDirection={["column", "row", "row", "row", "row"]}
-          transition={"opacity 0.2s ease-in 0s"}
-        >
-          <SocialIconComponent
-            href={"https://www.linkedin.com/in/moises-cruz-04531521b/"}
-            src={"/assets/icons/linkedin.svg"}
-            alt={"Ícone do linkedin"}
-            social={"Linkedin"}
-          />
-          <SocialIconComponent
-            href={"http://www.instagram.com/moisescrvz"}
-            src={"/assets/icons/instagram.svg"}
-            alt={"Ícone do instagram"}
-            social={"Instagram"}
-          />
-          <SocialIconComponent
-            href={"http://www.github.com/codemoises"}
-            src={"/assets/icons/github.svg"}
-            alt={"Ícone do github"}
-            social={"GitHub"}
-          />
-        </Flex>
-      </Box>
-    </>
+    <Box marginTop={"40px"}>
+      <Flex
+        className={"social"}
+        width={["auto", "auto", "550px", "550px", "auto"]}
+        justifyContent={"space-between"}
+        flexDirection={["column", "row", "row", "row", "row"]}
+        transition={"opacity 0.2s ease-in 0s"}
+      >
+        <SocialIconComponent
+          href={"https://www.linkedin.com/in/moises-cruz-04531521b/"}
+          src={"/assets/icons/linkedin.svg"}
+          alt={"Ícone do linkedin"}
+          social={"Linkedin"}
+        />
+        <SocialIconComponent
+          href={"http://www.instagram.com/moisescrvz"}
+          src={"/assets/icons/instagram.svg"}
+          alt={"Ícone do instagram"}
+          social={"Instagram"}
+        />
+        <SocialIconComponent
+          href={"http://www.github.com/codemoises"}
+          src={"/assets/icons/github.svg"}
+          alt={"Ícone do github"}
+          social={"GitHub"}
+        />
+      </Flex>
+    </Box>
   );
 }
