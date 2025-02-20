22ffb361d154a35527023a3e07cda5f7536d4a72
codemoises <88902469+codemoises@users.noreply.github.com>
2025-02-20 18:17:57 -0300

    remove white-line entre as seções

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,8 @@ export default function Index() {
   return (
     <>
       <Home />
-      <Box background={"#202020"} width={"100%"} height={"2px"}></Box>
       <About />
-      <Box background={"#202020"} width={"100%"} height={"2px"}></Box>
       <Projects />
-      <Box background={"#202020"} width={"100%"} height={"2px"}></Box>
       <Skills />
       <Footer />
     </>
